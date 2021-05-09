ecbeb93122bf5c40b4ccc82c161938d7da65442e
MichaKarlsruhe <michael.meinig@t-online.de>
2021-05-09 20:54:34 +0200

    Lightbox 2, Versuch 5

--- _includes/_lightbox.js
+++ _includes/_lightbox.js
@@ -2,7 +2,7 @@
 
 // Get the modal
 var modal = document.getElementById('myModal');
- 
+
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById('myImg');
 var modalImg = document.getElementById("img01");
@@ -13,8 +13,8 @@ img.onclick = function(){
     modalImg.alt = this.alt;
     captionText.innerHTML = this.alt;
 }
- 
- 
+
+
 // When the user clicks on <span> (x), close the modal
 modal.onclick = function() {
     img01.className += " out";
@@ -24,4 +24,4 @@ modal.onclick = function() {
      }, 400);
     
  }    
- 
+    
