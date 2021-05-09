1fced15f090205182d67ce46cd31e73b4ca2c537
MichaKarlsruhe <michael.meinig@t-online.de>
2021-05-09 19:02:03 +0200

    Lightbox Alternativ-Variante

--- _includes/_lightbox.js
+++ _includes/_lightbox.js
@@ -1,30 +1,27 @@
-var zoomImg = function () {
-    // (A) CREATE EVIL IMAGE CLONE
-    var clone = this.cloneNode();
-    clone.classList.remove("zoomD");
-  
-    // (B) PUT EVIL CLONE INTO LIGHTBOX
-    var lb = document.getElementById("lb-img");
-    lb.innerHTML = "";
-    lb.appendChild(clone);
-  
-    // (C) SHOW LIGHTBOX
-    lb = document.getElementById("lb-back");
-    lb.classList.add("show");
-  };
-  
-  window.addEventListener("load", function(){
-    // (D) ATTACH ON CLICK EVENTS TO ALL .ZOOMD IMAGES
-    var images = document.getElementsByClassName("zoomD");
-    if (images.length>0) {
-      for (var img of images) {
-        img.addEventListener("click", zoomImg);
-      }
-    }
-  
-    // (E) CLICK EVENT TO HIDE THE LIGHTBOX
-    document.getElementById("lb-back").addEventListener("click", function(){
-      this.classList.remove("show");
-    })
-  });
-  
+
+
+// Get the modal
+var modal = document.getElementById('myModal');
+ 
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+ 
+ 
+// When the user clicks on <span> (x), close the modal
+modal.onclick = function() {
+    img01.className += " out";
+    setTimeout(function() {
+       modal.style.display = "none";
+       img01.className = "modal-content";
+     }, 400);
+    
+ }    
+ 
